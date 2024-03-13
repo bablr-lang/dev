30df9d6252a51c749f91df99dffdbeefc30e4365
Conrad Buck <conartist6@gmail.com>
2024-03-13 12:02:23 -0600

    changes

--- play/index.js
+++ play/index.js
@@ -1,16 +1,41 @@
 /* global console, URL, globalThis */
 
-import { streamParseSync as streamParse } from 'bablr';
-import { printPrettyCSTML } from '@bablr/agast-helpers/stream';
+import tap from 'iter-tools-es/methods/tap';
+import { evaluate as agastEvaluate, Context as AgastContext } from '@bablr/agast-vm';
+import { evaluate, Context, Source } from '@bablr/bablr-vm';
+import { runSync } from '@bablr/agast-vm-helpers/run';
+import { printPrettyCSTML, printTerminal } from '@bablr/agast-helpers/stream';
 import { enhanceWithDebugLogging as log } from '@bablr/language_enhancer-debug-log';
-import indent from 'indent-string';
+import { enhanceStrategyWithDebugLogging as logStrategy } from '@bablr/strategy_enhancer-debug-log';
+import { createParseStrategy } from '@bablr/bablr-vm-strategy-parse';
 
 import { sourceText, language, matcher } from './fixture.js';
+import indent from 'indent-string';
+
+export { treeFromStreamSync, treeFromStreamAsync } from '@bablr/agast-helpers/tree';
+
+export function streamParse(language, sourceText, matcher, props) {
+  const agastCtx = AgastContext.create();
+  const ctx = Context.from(agastCtx, language);
+  const source = Source.from(sourceText);
+
+  return runSync(
+    agastEvaluate(
+      agastCtx,
+      evaluate(ctx, source, logStrategy(createParseStrategy(matcher, props), '  ')),
+    ),
+  );
+}
 
 console.log(`Input: \`${sourceText.replace(/[`\\]/g, '\\$&')}\``);
 
-const terminals = streamParse(log(language), sourceText, matcher);
+const terminals = streamParse(log(language, '    '), sourceText, matcher);
 
-const printed = printPrettyCSTML(terminals);
+const printed = printPrettyCSTML(
+  tap((terminal) => {
+    console.log('>>>', printTerminal(terminal));
+  }, terminals),
+);
 
+console.log();
 console.log(indent(printed, 4));
