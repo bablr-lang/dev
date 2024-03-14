57f03877d0da1a68cd7defa4fb04a13373b744d2
Conrad Buck <conartist6@gmail.com>
2024-03-14 09:40:15 -0600

    Refactor play

--- play/fixture.js
+++ play/fixture.js
@@ -3,4 +3,7 @@ import { spam } from '@bablr/boot';
 export * as language from '@bablr/language-cstml';
 
 export const sourceText = '<> </>';
+
 export const matcher = spam`<Tree>`;
+
+export const props = {};
--- play/index.js
+++ play/index.js
@@ -9,28 +9,24 @@ import { enhanceWithDebugLogging as log } from '@bablr/language_enhancer-debug-l
 import { enhanceStrategyWithDebugLogging as logStrategy } from '@bablr/strategy_enhancer-debug-log';
 import { createParseStrategy } from '@bablr/bablr-vm-strategy-parse';
 
-import { sourceText, language, matcher } from './fixture.js';
+import { sourceText, language, matcher, props } from './fixture.js';
 
 export { treeFromStreamSync, treeFromStreamAsync } from '@bablr/agast-helpers/tree';
-
-export function streamParse(language, sourceText, matcher, props) {
-  const agastCtx = AgastContext.create();
-  const ctx = Context.from(agastCtx.facade, language);
-  const source = Source.from(sourceText);
-
-  return runSync(
-    agastEvaluate(
-      agastCtx,
-      logStrategy(evaluate(ctx, source, createParseStrategy(matcher, props)), '  '),
-    ),
-  );
-}
 
 console.log();
 console.log(`Input: \`${sourceText.replace(/[`\\]/g, '\\$&')}\``);
 console.log();
 
-const terminals = streamParse(log(language, '    '), sourceText, matcher);
+const agastCtx = AgastContext.create();
+const ctx = Context.from(agastCtx.facade, log(language, '    '));
+const source = Source.from(sourceText);
+
+const terminals = runSync(
+  agastEvaluate(
+    agastCtx,
+    logStrategy(evaluate(ctx, source, createParseStrategy(matcher, props)), '  '),
+  ),
+);
 
 const printed = printPrettyCSTML(
   tap((terminal) => {
