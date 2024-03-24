50000d3cb5989abd2aad908c2e171cd7d9a612ca
Conrad Buck <conartist6@gmail.com>
2024-03-24 09:35:56 -0600

    fixup

--- play/index.js
+++ play/index.js
@@ -1,15 +1,19 @@
-/* global console, URL, globalThis */
+/* global console, URL, globalThis, process */
 
+import ansi from 'ansi';
 import tap from 'iter-tools-es/methods/tap';
 import map from 'iter-tools-es/methods/map';
+import { buildFullyQualifiedSpamMatcher } from '@bablr/agast-vm-helpers/builders';
 import { evaluate as agastEvaluate, Context as AgastContext } from '@bablr/agast-vm';
 import { evaluate, Context, Source } from '@bablr/bablr-vm';
+import { streamParse } from 'bablr';
 import { runSync } from '@bablr/agast-vm-helpers/run';
 import { buildDependentLanguages } from '@bablr/helpers/grammar';
 import { printPrettyCSTML, printTerminal } from '@bablr/agast-helpers/stream';
 import { enhanceWithDebugLogging as log } from '@bablr/language_enhancer-debug-log';
 import { enhanceStrategyWithDebugLogging as logStrategy } from '@bablr/strategy_enhancer-debug-log';
 import { createParseStrategy } from '@bablr/bablr-vm-strategy-parse';
+import * as cstml from '@bablr/language-cstml';
 
 import { sourceText, language, matcher, props } from './fixture.js';
 
@@ -44,4 +48,43 @@ const printed = printPrettyCSTML(
 );
 
 console.log();
-console.log(printed);
+
+const printColorfulCSTML = () => {
+  const spamFragment = buildFullyQualifiedSpamMatcher(cstml.canonicalURL, 'Fragment');
+
+  const output = ansi(process.stdout);
+
+  output.red();
+
+  let isString = false;
+
+  for (const token of runSync(streamParse(buildDependentLanguages(cstml), printed, spamFragment))) {
+    if (token.type === 'OpenNodeTag') {
+      if (token.value.type === 'String') {
+        isString = true;
+        output.green();
+      } else if (!isString) {
+        if (['Punctuator', 'Keyword'].includes(token.value.type)) {
+          output.red();
+        }
+      }
+    }
+
+    if (token.type === 'CloseNodeTag') {
+      if (token.value.type === 'String') {
+        isString = false;
+      }
+      if (!isString) {
+        output.reset();
+      }
+    }
+
+    if (token.type === 'Literal') {
+      output.write(token.value);
+    }
+  }
+
+  output.reset().write('\n');
+};
+
+printColorfulCSTML(printed);
--- scripts/use-https-submodules.js
+++ scripts/use-https-submodules.js
@@ -7,6 +7,7 @@ import { local } from './utils/path.js';
 const modulesFileContent = await readFile(local`.gitmodules`, 'utf8');
 
 await writeFile(
+  local`.gitmodules`,
   modulesFileContent.replace(/(\s*url\s*=\s*)git@github.com:(.*)/g, '$1https://github.com/$2'),
 );
 
