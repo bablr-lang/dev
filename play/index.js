1226057fcce0cc0afd3c7732cb76513487545db3
Conrad Buck <conartist6@gmail.com>
2024-07-25 11:32:53 -0600

    language-en

--- play/index.js
+++ play/index.js
@@ -12,23 +12,38 @@ import {
 } from '@bablr/agast-helpers/tree';
 import { evaluateIO } from '@bablr/io-vm-node';
 import { printPrettyCSTML as printCSTMLStream } from '@bablr/agast-helpers/stream';
-// import * as language from '@bablr/language-es3';
-// import * as language from '@bablr/language-cstml';
-import * as language from '@bablr/language-regex-vm-pattern';
-// import * as language from '@bablr/language-bablr-vm-instruction';
+import * as language from '@bablr/language-en-c-comments';
+import { generatePrettyCSTMLStrategy } from '@bablr/agast-helpers/stream';
+// import * as language from '@bablr/language-en-cstml';
+// import * as language from '@bablr/language-en-regex-vm-pattern';
+// import * as language from '@bablr/language-en-bablr-vm-instruction';
+// import * as language from '@bablr/language-en-bablr-cli-verbose-output';
 
 // import { sourceText, language, type, props } from './fixture.js';
-import { createPrintCSTMLStrategy } from '@bablr/cli/syntax';
+// import { createPrintCSTMLStrategy } from '@bablr/cli/syntax';
+import { evaluateIO } from '@bablr/io-vm-node';
 
 global.__printSource = printSource;
 
-const tag = buildTag(language, 'Pattern', null, debugEnhancers);
+let enhancers = {};
 
-// const tokens = [...streamParse(language, 'Fragment', `<><Node></></>`, {}, debugEnhancers)];
+enhancers = { ...debugEnhancers, enhancers };
 
-const tree = tag`/a+/`;
-const tokens = streamFromTree(tree);
+// const tag = buildTag(language, 'Pattern', null, debugEnhancers);
+
+const { tokens } = streamParse(
+  language,
+  'Trivia',
+  String.raw` `,
+  {},
+  { enhancers, emitEffects: true },
+);
+
+// const tree = tag`/a+/`;
+// const tokens = streamFromTree(tree);
 
 console.log();
 
-evaluateIO(createPrintCSTMLStrategy(tokens, { color: false, format: true }), process.stdout);
+evaluateIO(createPrintCSTMLStrategy(tokens, { color: false, format: true, emitEffects: true }));
+
+// evaluateIO(() => generatePrettyCSTMLStrategy(tokens));
