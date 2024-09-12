7853ae38072358752b222261d6dfe2b906aa4e23
Stirling Hostetter <stirlhoss@proton.me>
2024-09-12 11:17:08 -0500

    update play index

--- play/index.js
+++ play/index.js
@@ -1,48 +1,26 @@
 /* global global, console, URL, globalThis, process */
 
-import { pipeline } from 'node:stream/promises';
-import { createWriteStream } from 'node:fs';
-import indent from 'indent-string';
-import { streamParse, buildTag } from 'bablr/enhanceable';
+import { streamParse, buildTag, Context, AgastContext } from 'bablr/enhanceable';
 import { debugEnhancers } from '@bablr/helpers/enhancers';
-import {
-  streamFromTree,
-  printPrettyCSTML as printPrettyCSTMLFromTree,
-  printSource,
-} from '@bablr/agast-helpers/tree';
 import { evaluateIO } from '@bablr/io-vm-node';
-import { printPrettyCSTML as printCSTMLStream } from '@bablr/agast-helpers/stream';
-import * as language from '@bablr/language-en-c-comments';
-import { generatePrettyCSTMLStrategy } from '@bablr/agast-helpers/stream';
 // import * as language from '@bablr/language-en-cstml';
-// import * as language from '@bablr/language-en-regex-vm-pattern';
-// import * as language from '@bablr/language-en-bablr-vm-instruction';
-// import * as language from '@bablr/language-en-bablr-cli-verbose-output';
+import * as language from '@bablr/language-en-json';
+import { generatePrettyCSTMLStrategy } from '@bablr/helpers/stream';
+import { printPrettyCSTML } from '@bablr/helpers/tree';
 
-// import { sourceText, language, type, props } from './fixture.js';
-import { createPrintCSTMLStrategy } from '@bablr/cli/syntax';
-
-global.__printSource = printSource;
+import { buildFullyQualifiedSpamMatcher } from '@bablr/agast-vm-helpers';
 
 let enhancers = {};
 
 enhancers = { ...debugEnhancers, enhancers };
 
-// const tag = buildTag(language, 'Pattern', null, debugEnhancers);
+const input = `"'"`;
 
-const { tokens } = streamParse(
-  language,
-  'Trivia',
-  String.raw` `,
-  {},
-  { enhancers, emitEffects: true },
-);
+const matcher = buildFullyQualifiedSpamMatcher({}, language.canonicalURL, 'String');
+const ctx = Context.from(AgastContext.create(), language, enhancers.bablrProduction);
 
-// const tree = tag`/a+/`;
-// const tokens = streamFromTree(tree);
+const tokens = streamParse(ctx, matcher, input, {}, { enhancers, emitEffects: true });
 
 console.log();
 
-evaluateIO(createPrintCSTMLStrategy(tokens, { color: false, format: true, emitEffects: true }));
-
-// evaluateIO(() => generatePrettyCSTMLStrategy(tokens));
+evaluateIO(() => generatePrettyCSTMLStrategy(tokens, { ctx, emitEffects: true }));
