00a7fa89d631c777cd5bd260e7c9b19fe81d24aa
Conrad Buck <conartist6@gmail.com>
2025-01-21 09:29:17 -0700

    wip

--- play/index.js
+++ play/index.js
@@ -1,19 +1,21 @@
 /* global global, console, URL, globalThis, process */
 
 import { spam, str, spam as m } from '@bablr/boot';
-import { streamParse, buildTag, Context, AgastContext } from 'bablr/enhanceable';
+import { streamParse, buildTag, Context } from 'bablr/enhanceable';
 import { debugEnhancers } from '@bablr/helpers/enhancers';
 import { evaluateIO } from '@bablr/io-vm-node';
-// import * as language from '@bablr/language-en-cstml';
 import * as language from '@bablr/language-en-regex-vm-pattern';
+// import * as language from '@bablr/language-en-json';
 import { generatePrettyCSTMLStrategy } from '@bablr/helpers/stream';
 import { printPrettyCSTML } from '@bablr/helpers/tree';
 
-import { streamFromTree } from '@bablr/agast-helpers/tree';
+import { printTag, streamFromTree } from '@bablr/agast-helpers/tree';
 import { embeddedSourceFrom } from '@bablr/helpers/source';
-import { buildString } from '@bablr/agast-vm-helpers';
+import { buildIdentifier, buildString } from '@bablr/agast-vm-helpers';
 
 let enhancers = {};
+
+global.printTag = printTag;
 
 enhancers = { ...debugEnhancers, enhancers };
 
@@ -23,36 +25,16 @@ enhancers = { ...debugEnhancers, enhancers };
 //     '<//>': 5555
 //   }'`);
 
-const input = String.raw`/a/`;
+const input = String.raw`/[--]/`;
 
 const matcher = spam`<$${buildString(language.canonicalURL)}:Pattern />`;
-const ctx = Context.from(AgastContext.create(), language, enhancers.bablrProduction);
-
+const ctx = Context.from(language, enhancers.bablrProduction);
 const tokens = streamParse(ctx, matcher, input, {}, { enhancers, emitEffects: true });
 
 console.log();
 
 evaluateIO(() => generatePrettyCSTMLStrategy(tokens, { ctx, emitEffects: true }));
 
-// const tag = buildTag(ctx, matcher);
+// const tag = buildTag(ctx, matcher, undefined, enhancers);
 
-// console.log(printPrettyCSTML(tag`null`, { ctx }));
-
-// const { raw } = String;
-
-// enhancers = debugEnhancers;
-
-// const ctx = Context.from(AgastContext.create(), language, enhancers.bablrProduction);
-
-// const buildCSTMLTag = (type) => {
-//   const matcher = spam`<$${buildString(language.canonicalURL)}:${buildString(type)} />`;
-//   return buildTag(ctx, matcher, undefined, { enhancers });
-// };
-
-// const print = (tree) => {
-//   return printPrettyCSTML(tree, { ctx });
-// };
-
-// const cstml = buildCSTMLTag('Node');
-
-// print(cstml`<Node>reference: null</>`);
+// console.log(printPrettyCSTML(tag`"'"`, { ctx }));
