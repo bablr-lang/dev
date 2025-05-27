23016e8c9c28e55bf67f4ef66651b84e7e290cdb
Stirling Hostetter <stirlhoss@proton.me>
2025-05-27 14:34:42 -0500

    update play file

--- play/index.js
+++ play/index.js
@@ -1,13 +1,14 @@
 /* global global, console, URL, globalThis, process */
 
 import { spam, str, i } from '@bablr/boot';
-import { streamParse, buildTag, Context } from 'bablr/enhanceable';
+import { streamParse, buildTag } from 'bablr/enhanceable';
 import { debugEnhancers } from '@bablr/helpers/enhancers';
 import { evaluateIO } from '@bablr/io-vm-node';
 // import * as language from '@bablr/language-en-regex-vm-pattern';
 // import * as language from '@bablr/language-en-json';
 // import * as language from '@bablr/language-en-scheme';
-import * as language from '@bablr/language-en-ruby';
+// import * as language from '@bablr/language-en-ruby';
+import * as language from 'language-en-es6';
 
 import { printPrettyCSTML as printPrettyCSTMLStream } from '@bablr/agast-helpers/stream';
 import { printPrettyCSTML } from '@bablr/helpers/tree';
@@ -27,19 +28,18 @@ enhancers = { ...debugEnhancers, enhancers };
 const input = String.raw`"hello"`;
 // const input = embeddedSourceFrom(`'[ '<//>' ]'`);
 
-const matcher = spam`<$${buildString(language.canonicalURL)}:Program />`;
-const ctx = Context.from(language, enhancers.bablrProduction);
+const matcher = spam`<$Program />`;
 
 const tags = evaluateIO(() =>
-  streamParse(ctx, matcher, input, {}, { enhancers, emitEffects: true }),
+  streamParse(language, matcher, input, {}, { enhancers, emitEffects: true }),
 );
 
 console.log();
 
 //evaluateReturnSync(tags);
 
-console.log(printPrettyCSTMLStream(tags, { ctx }));
-// const tag = buildTag(ctx, matcher, undefined, enhancers);
+console.log(printPrettyCSTMLStream(tags));
+// const tag = buildTag(language, matcher, undefined, enhancers);
 
 // const flags = tag.Flags`i`;
-// console.log(printPrettyCSTML(tag`${flags}`, { ctx }));
+// console.log(printPrettyCSTML(tag`${flags}`));
